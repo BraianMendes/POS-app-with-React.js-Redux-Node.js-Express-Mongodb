cb5cf96a310988707781a9cb49e463330bfb0d1f
Braian <00.braian.dev@gmail.com>
2020-07-23 20:10:16 -0300

    Braian: Starting Backend

---
+++ src/routes/api.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const router = express.Router()
+require("../utils/db")
+router.use(require("./api_auth"))
+{/* <em class="markup--em markup--pre-em">// router.use(require("./api_product"))
+</em><em class="markup--em markup--pre-em">// router.use(require("./api_employee"))
+</em><em class="markup--em markup--pre-em">// router.use(require("./api_customer"))
+</em><em class="markup--em markup--pre-em">// router.use(require("./api_pos_machine"))
+</em><em class="markup--em markup--pre-em">// router.use(require("./api_branch"))
+</em> */}
+module.exports = router
